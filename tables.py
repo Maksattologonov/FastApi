cd8d9daddf1bd78f257e85c3684357ea57fa541d
Maksattologonov <Maksattologonovn@gmail.com>
2021-09-25 20:28:55 +0600

    alembic and migrations added

--- tables.py
+++ tables.py
@@ -1,6 +1,23 @@
+from decimal import Decimal
+from enum import Enum
+from datetime import date
+from typing import Optional
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
+class OperationKind(str, Enum):
+    INCOME = 'income'
+    OUTCOME = 'outcome'
+
+
 class Operation(Base):
     __tablename__ = 'operations'
+
+    id: int
+    date: date
+    kind: OperationKind
+    amount: Decimal
+    description: Optional[str]
